dd5477c0e1e9875a84c8c78f701865a43519887a
DRC <hello@duke-robotics.com>
2024-04-10 20:37:46 -0400

    Revert

--- Datasets/bbox_filter.py
+++ Datasets/bbox_filter.py
@@ -17,7 +17,7 @@ class ImageWithBBoxes:
         self.max_area = float("-inf")
 
         for bbox in bboxes:
-            if bbox["area"] >= self.max_area and bbox["category_id"] != 6:
+            if bbox["area"] >= self.max_area:
                 self.max_area = bbox["area"]
             self.bboxes[bbox["id"]] = bbox
 
