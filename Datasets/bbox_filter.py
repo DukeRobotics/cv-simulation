275fccbda6832e39477766051114c0c70c18b8a8
DRC <hello@duke-robotics.com>
2024-04-20 14:14:39 -0400

    Update bbox filter

--- Datasets/bbox_filter.py
+++ Datasets/bbox_filter.py
@@ -1,34 +1,32 @@
-
 import json
 import argparse
 import pathlib
-
 from collections import defaultdict
 
 DATASETS_PATH = pathlib.Path(__file__).parent.resolve()
 
 class ImageWithBBoxes:
-
-    # Default threshold is to eliminate bounding boxes less than 10% of the maximum size bounding box
-    def __init__(self, bboxes, threshold=0.1):
-
-        self.threshold = threshold
+    def __init__(self, bboxes):
         self.bboxes = defaultdict(dict)
-        self.max_area = float("-inf")
+        self.max_area_by_category = {}
 
         for bbox in bboxes:
-            if bbox["area"] >= self.max_area:
-                self.max_area = bbox["area"]
+            if (category_id := bbox["category_id"]) not in self.max_area_by_category:
+                self.max_area_by_category[category_id] = bbox["area"]
+            else:
+                self.max_area_by_category[category_id] = max(self.max_area_by_category[category_id], bbox["area"])
             self.bboxes[bbox["id"]] = bbox
-
-        # Filter/remove all bounding boxes less than threshold% of the max_area bounding box        
+      
         self.filter()
 
     def filter(self):
-        # remove = [self.bboxes[bbox]["id"] for bbox in self.bboxes if self.bboxes[bbox]["area"] <= (0.1 * self.max_area) or self.bboxes[bbox]["area"] == 1]
-        remove = [self.bboxes[bbox]["id"] for bbox in self.bboxes if self.bboxes[bbox]["area"] == 1]
+        remove = []
+        for id, bbox in self.bboxes.items():
+            # Remove bboxes with area 1 or smaller duplicates
+            if bbox["area"] == 1 or bbox["area"] < self.max_area_by_category[bbox["category_id"]]:
+                remove.append(id)
+
         for id in remove:
-            print(f"Removed id {id}")
             del self.bboxes[id]
 
     def export(self):
@@ -36,10 +34,7 @@ class ImageWithBBoxes:
 
 
 def filter(filename: pathlib.Path):
-
-    # Open the JSON file for reading
     with open(filename, "r") as file:
-
         # Parse the JSON file and convert it into a Python dictionary
         superdirectory = json.load(file)
         annotations = superdirectory["annotations"]
@@ -54,20 +49,20 @@ def filter(filename: pathlib.Path):
         for id in images_to_bbox:
             images.append(ImageWithBBoxes(images_to_bbox[id]))
 
+        # Export each image
         export_jsons = []
         for image in images:
             bbox_list = image.export()
             export_jsons.extend(bbox_list)
-
         superdirectory["annotations"] = export_jsons
+
         # Write the dictionary to a JSON file
         with open(filename, "w") as file:
             json.dump(superdirectory, file, indent=4)
-        print("Success")
         
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Upload a dataset to Roboflow.")
+    parser = argparse.ArgumentParser(description="Filter COCO bounding boxes.")
     parser.add_argument("dataset_path", help="Path to the COCO dataset to filter.", type=pathlib.Path)
 
     args = parser.parse_args()
